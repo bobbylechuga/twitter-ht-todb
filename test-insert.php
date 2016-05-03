c8cf2b3f588535ace2f92a97cfb829039faae25b
Pino <pino.jose@gmail.com>
2016-05-03 20:54:42 +0000

    agregado insert a la BD

--- test-insert.php
+++ test-insert.php
@@ -1,7 +1,16 @@
-<?php
+﻿<?php
 ini_set('display_errors', 1);
+//mysql_query("set names 'utf8'");
 require_once('twitter-api-php/TwitterAPIExchange.php');
 require_once('conf.inc.php');
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+$table = "votos";
+$socialType = 0; 
+if ($conn->connect_error) {
+	die("Connection fallida: " . $conn->connect_error);
+} 
 
 $settings = array(
         'oauth_access_token' => "$access_token",
@@ -11,12 +20,28 @@ $settings = array(
 );
 $requestMethod = 'GET';
 $url = 'https://api.twitter.com/1.1/search/tweets.json';
-$getfield = '?q=#DiaLibertadPrensa';
+$getfield = '?q=#YaNoPuedoCreerEn-filter:retweets&result_type=recent&true&count=100';
+//$getfield = '?q=#YaNoPuedoCreerEn';
 
 $twitter = new TwitterAPIExchange($settings);
 $response = $twitter->setGetfield($getfield)
     ->buildOauth($url, $requestMethod)
     ->performRequest();
-
-var_dump($response);
+	$cont = 0;
+	$parsed_json = json_decode($response);
+	foreach($parsed_json->statuses as $k) { 
+		$usuario[] = $k->user;
+		foreach($usuario as $q) {
+			$idUser = $q->id;
+		}
+		$cont++;
+		$tweetDate = date('Y-m-d H:i:s', strtotime($k->created_at));
+		//echo "ID:".$k->id." Tweet: ".$k->text."<br>";
+		$insert = "insert into ".$table." (id, message, date_social,  date_current, FB_post_id, voto_id, user_id, social_type) ".
+				  "VALUES ('', '$k->text', '$tweetDate', now(), '$k->id', '$k->id_str', '$idUser', '$socialType')"; 
+		mysqli_query($conn, $insert);
+		echo $insert. "<br>";
+		
+	}
+//print_r($response);
 ?>
