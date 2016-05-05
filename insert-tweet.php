9d717b30616a443e2aca05a12d8da3391e38f861
Pino <pino.jose@gmail.com>
2016-05-05 13:48:16 -0300

    credo script para 3 etiquetas

--- insert-tweet.php
+++ insert-tweet.php
@@ -1,10 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-date_default_timezone_set('America/Argentina/Buenos_Aires');
-//mysql_query("set names 'utf8'");
+//ini_set('display_errors', 1);
 require_once('twitter-api-php/TwitterAPIExchange.php');
 require_once('conf.inc.php');
-
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 $table = "votos";
@@ -19,18 +16,27 @@ $settings = array(
         'consumer_key' => "$consumer_key",
         'consumer_secret' => "$consumer_secret"
 );
-$requestMethod = 'GET';
-$url = 'https://api.twitter.com/1.1/search/tweets.json';
-$getfield = '?q=#DichosDeChilenos-filter:retweets&result_type=recent&true&count=100';
-//$getfield = '?q=#NoAlMaltratoAnimal';
 
-$twitter = new TwitterAPIExchange($settings);
-$response = $twitter->setGetfield($getfield)
+$url = 'https://api.twitter.com/1.1/search/tweets.json';
+$requestMethod = 'GET';
+
+//$getfield = '?q=#FelizMartes+AND+#testhht';
+//$tagPermanente = "#FelizMartes";
+$hastags = array ("#MarchaCones", "#TodosSomosMagnificos", "#libertadcondicional"); 
+$arrlength = count($hastags);
+
+for($x = 0; $x < $arrlength; $x++) {
+    //$strUrl = "?q=".$tagPermanente."+AND+".$hastags[$x];
+	//echo $url."?q=".$tagPermanente."+AND+".$hastags[$x]."<br>";
+	$strUrl = '?q='.$hastags[$x].'-filter:retweets&result_type=recent&true&count=100';
+	//echo $strUrl;
+	$twitter = new TwitterAPIExchange($settings);
+    $response = $twitter->setGetfield($strUrl)
     ->buildOauth($url, $requestMethod)
     ->performRequest();
-	$cont = 0;
+
 	$parsed_json = json_decode($response);
-	
+
 	foreach($parsed_json->statuses as $k) { 
 		$usuario[] = $k->user;
 		foreach($usuario as $q) {
@@ -43,12 +49,13 @@ $response = $twitter->setGetfield($getfield)
 		//$tweetDate = $k->created_at;
 		//echo "ID:".$k->id." Tweet: ".$k->text."<br>";
 		$tweet = utf8_decode ($k->text); 
-		$insert = "insert into ".$table." (id, message, date_social,  date_current, FB_post_id, voto_id, user_id, social_type) ".
-				  "VALUES ('', '$tweet', '$tweetDate', '$now', '$k->id', '$k->id_str', '$idUser', '$socialType')"; 
+		$insert = "insert into ".$table." (id, message, date_social,  date_current, FB_post_id, voto_id, user_id, etiqueta_id, social_type) ".
+				  "VALUES ('', '$tweet', '$tweetDate', '$now', '$k->id', '$k->id_str', '$idUser', $x, '$socialType')"; 
 		
-		//mysqli_query($conn, $insert); 
-		echo $insert. "<br>";
-		
-	}
+		mysqli_query($conn, $insert); 
+		//echo $insert. "<br>";
+	} 
+}
+
 //print_r($response);
 ?>
